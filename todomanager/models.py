6094ca8f450a930c2b1d60e966fcf889eadc240e
Mika Vainio <koti.mika.vainio@gmail.com>
2021-10-25 10:12:59 +0300

    Tehtävälle tehty-kenttä

--- todomanager/models.py
+++ todomanager/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Tehtava(models.Model):
     otsikko = models.CharField(max_length=250)
     kuvaus = models.CharField(max_length=2000, default="", blank=True)
+    tehty = models.BooleanField(default=False)
 
     def __str__(self):
         return self.otsikko
