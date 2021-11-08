086d93867c46321a49423f1663f3bd68a76737d8
Mika Vainio <koti.mika.vainio@gmail.com>
2021-11-08 15:05:39 +0200

    Vaihda tietokannaksi PostgreSQL

--- todomanager/settings.py
+++ todomanager/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'todomanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        # 'PORT': 5432,
+        'USER': 'todoapp',
+        'PASSWORD': 'todoapp',
+        'NAME': 'todoapp',
     }
 }
 
