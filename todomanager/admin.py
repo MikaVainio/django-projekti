26410c1004e47e30e273f473e00d873fe2392cf2
Mika Vainio <koti.mika.vainio@gmail.com>
2021-11-08 15:05:20 +0200

    Lisää kategoria

--- todomanager/admin.py
+++ todomanager/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Tehtava
-
+from .models import Kategoria, Tehtava
 
 
 @admin.register(Tehtava)
 class TehtavaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Kategoria)
+class KategoriaAdmin(admin.ModelAdmin):
     pass
--- todomanager/models.py
+++ todomanager/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
+class Kategoria(models.Model):
+    nimi = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nimi
+
+
 class Tehtava(models.Model):
     otsikko = models.CharField(max_length=250)
     kuvaus = models.CharField(max_length=2000, default="", blank=True)
     tehty = models.BooleanField(default=False)
+    kategoria = models.ForeignKey(
+        Kategoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.otsikko
